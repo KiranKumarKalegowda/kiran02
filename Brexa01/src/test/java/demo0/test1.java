f51ce07ef1c6ca73c99c2a198e90446de7042032
Mr. Kiran K <Mr. Kiran K@DESKTOP-3BO9971>
2022-05-09 15:26:37 +0530

    bye

---
+++ Brexa01/src/test/java/demo0/test1.java
@@ -0,0 +1,7 @@
+package demo0;
+
+public class test1 {
+public static void main(String[] args) {
+	System.out.println("hi");
+}
+}
---
+++ Brexa01/src/test/java/demo0/text02.java
@@ -0,0 +1,7 @@
+package demo0;
+
+public class text02 {
+public static void main(String[] args) {
+	System.out.println("hai hello man how are you");
+}
+}
